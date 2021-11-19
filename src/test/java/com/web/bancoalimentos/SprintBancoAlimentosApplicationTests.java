3cd91326b8656a21ed764064361e33930848860a
carlos <cjayala-rincon@misena.edu.co>
2021-11-19 10:47:02 -0500

    creacion de la estructura y clases del proyecto

---
+++ src/test/java/com/web/bancoalimentos/SprintBancoAlimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.bancoalimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBancoAlimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
