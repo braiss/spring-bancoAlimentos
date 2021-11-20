e77870540297065d0bcb17138ef6288d1cfe252c
carlos <cjayala-rincon@misena.edu.co>
2021-11-20 00:25:27 -0500

    implementacion para guardar un producto desde la pagina a la base de datos

--- src/main/java/com/web/bancoalimentos/controller/ProductoController.java
+++ src/main/java/com/web/bancoalimentos/controller/ProductoController.java
@@ -1,18 +1,23 @@
 package com.web.bancoalimentos.controller;
 
 import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.web.bancoalimentos.model.Producto;
+import com.web.bancoalimentos.model.Usuario;
+import com.web.bancoalimentos.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 	
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	@Autowired
+	private ProductoService productoService;
 	
 	@GetMapping("")
 	public String show() { 
@@ -30,6 +35,9 @@ public class ProductoController {
 		
 		LOGGER.info("este es el objeto de producto {}",producto);
 		
+		Usuario u= new Usuario(1,"","","","","","", "");
+		producto.setUsuario(u);
+		productoService.save(producto);
 		return "redirect:/productos";
 	}
 
