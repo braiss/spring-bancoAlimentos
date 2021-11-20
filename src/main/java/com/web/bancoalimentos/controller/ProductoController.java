95d7096fafb987ad56ec709ed0a516dfacf16432
carlos <cjayala-rincon@misena.edu.co>
2021-11-19 22:22:36 -0500

    implementacion del guardado de productos e img

--- src/main/java/com/web/bancoalimentos/controller/ProductoController.java
+++ src/main/java/com/web/bancoalimentos/controller/ProductoController.java
@@ -1,12 +1,18 @@
 package com.web.bancoalimentos.controller;
 
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.web.bancoalimentos.model.Producto;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
 	
 	@GetMapping("")
 	public String show() { 
@@ -18,6 +24,13 @@ public class ProductoController {
 		
 		return "productos/create";
 		
+	}
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		
+		LOGGER.info("este es el objeto de producto {}",producto);
+		
+		return "redirect:/productos";
 	}
 
 }
